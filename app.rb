ba0da107ff990f5b1f9d5d0a76c239e47dd94a50
Adrienne Yao <adrienne.yao@icloud.com>
2019-09-27 01:07:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/reverse' do
+
     erb :reverse
   end
 
   post '/reverse' do
+
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    
     erb :reversed
   end
 
